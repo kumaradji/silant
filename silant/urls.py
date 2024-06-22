a067ab813fe2a5471b0a1b05a22fa4c3ce252acc
kumaradji <kumaradji@gmail.com>
2024-06-22 21:24:23 +0300

    +++ БД

--- silant/urls.py
+++ silant/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from inventory import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,4 @@ urlpatterns = [
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', LogoutView.as_view(next_page='welcome'), name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
