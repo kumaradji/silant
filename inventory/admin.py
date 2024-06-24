536fe21809b760c3fb5cf69f97abd49e28c0adbe
kumaradji <kumaradji@gmail.com>
2024-06-24 21:07:38 +0300

    _____

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,12 +1,24 @@
-# inventory/admin.py
-
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Machine, Maintenance, Reclamation
+
+
+class MachineAdminForm(forms.ModelForm):
+    class Meta:
+        model = Machine
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(MachineAdminForm, self).__init__(*args, **kwargs)
+        self.fields['customer'].queryset = User.objects.all()
+        self.fields['consignee'].queryset = User.objects.all()
 
 
 @admin.register(Machine)
 class MachineAdmin(admin.ModelAdmin):
-    list_display = ('serial_number', 'model_technique', 'model_engine', 'customer', 'service_company')
+    form = MachineAdminForm
+    list_display = ('serial_number', 'model_technique', 'model_engine', 'customer', 'consignee', 'service_company')
     search_fields = ('serial_number', 'model_technique', 'model_engine')
     list_filter = ('model_technique', 'model_engine', 'customer', 'service_company')
 
